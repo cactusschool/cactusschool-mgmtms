44aab97e821206850d077844f3e83057f25e6996
ashismo <ashishkrmondal@gmail.com>
2019-02-10 22:10:26 -0500

    update script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 git pull
-notice_process=`ps -ef | grep cactusschool-noticems | awk '$8=="java" {print $2}'`
-echo $notice_process
-kill -9 $notice_process
+mgmt_process=`ps -ef | grep cactusschool-mgmtms | awk '$8=="java" {print $2}'`
+echo $mgmt_process
+kill -9 $mgmt_process
 echo "Running application has been stopped. Starting the application again"
 mvn -DskipTests=true clean install
-java -jar target/cactusschool-noticems-1.0.jar &
+java -jar target/cactusschool-mgmtms-1.0.jar &
